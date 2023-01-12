dc870534a3f7cb05aa1b4654fcaf89298f398497
mamoune.bellatif <mamoune.bellatif@businessdecision.com>
2023-01-12 14:12:26 +0100

    first commit

---
+++ src/main/java/fr/miage/GroMed/GroMedApplication.java
@@ -0,0 +1,13 @@
+package fr.miage.GroMed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroMedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroMedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/miage/GroMed/GroMedApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.GroMed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
