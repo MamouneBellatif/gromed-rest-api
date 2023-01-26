8f35ff4c223fb43be4d4cdb125d02f04e29da56a
Amel LOUNIS <amellounis@eduroam-115073.grenet.fr>
2023-01-26 10:46:57 +0100

    anier server

--- src/main/java/fr/miage/gromed/service/PanierService.java
+++ src/main/java/fr/miage/gromed/service/PanierService.java
@@ -1,16 +1,23 @@
 package fr.miage.gromed.service;
 
+import fr.miage.gromed.dto.MedicamentDto;
+import fr.miage.gromed.dto.PanierDto;
+import fr.miage.gromed.dto.PresentationDto;
 import fr.miage.gromed.model.Panier;
+import fr.miage.gromed.model.medicament.Presentation;
 import fr.miage.gromed.repositories.PanierElementRepository;
 import fr.miage.gromed.repositories.PanierRepository;
 import jakarta.persistence.LockModeType;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PanierService {
+public class
+PanierService {
 
     @Autowired
     private PanierRepository panierRepository;
@@ -23,13 +30,39 @@ public class PanierService {
 
     @Transactional
     @Lock(LockModeType.OPTIMISTIC)
-    public void updatePanier(long productId, int quantity) {
-        Panier panier = panierRepository.findById(productId).get();
+    public PanierDto updatePanier(int codeCIP, int quantite, Presentation presentation, long idPanier) {
+        Panier panier = panierRepository.findById(idPanier).get();
         panier.getItems().forEach(panierItem -> {
 //            panierItem.getPresentation().
-            stockService.updateStock(panierItem.getPresentation().getId(), panierItem.getQuantite());
-        panierItem.setQuantite(panierItem.getQuantite() + quantity);
+            stockService.updateStock(panierItem.getId(), panierItem.getQuantite());
         });
+      
+         panierRepository.save(panier);
+    }
+    
+
+    public PanierDto getPanier(long idPanier) {
+        Panier panier = panierRepository.findById(idPanier).get();
+        PanierDto panierDto = new PanierDto();
+        panierDto.setId(panier.getId());
+        panierDto.setItems(panier.getItems());
+        return panierDto;
+    }
+    
+    public ResponseEntity<PanierDto> delete(long idPanier) {
+        Panier panier = panierRepository.findById(idPanier).get();
+        panier.getItems().forEach(panierItem -> {
+            stockService.updateStock(panierItem.getId(), panierItem.getQuantite());
+        });
+        panierRepository.delete(panier);
+    }
+
+    @Transactional
+    public ResponseEntity<PanierDto> addItemsToPanier(int idPanier, PresentationDto presentationDto) {
+        Panier panier = panierRepository.findById(idPanier);
+        PresentationDto presentation = mapDtoToPresentation(presentationDto);
+        panier.addItemsToPanier(presentation);
         panierRepository.save(panier);
+        return new ResponseEntity<>(mapPanierToDto(panier), HttpStatus.OK);
     }
 }
